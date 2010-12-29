88fbcd627ebc1a6854d82c566acfcfe10d745e8b
Don Park <don@donpark.org>
2010-12-29 11:18:17 -0800

    fix new style geoloqi battery

--- jsview/people.js
+++ jsview/people.js
@@ -215,8 +215,8 @@ var People = {
     me.fadeOut();
     me.html(People.time_ago(last_date));
     me.fadeIn();
-    if(typeof(json.data.raw)!="undefined") {
-      $('#'+user.username+'_battery').html(json.data.raw.battery+'% batt');
+    if(typeof(json.raw.battery)!="undefined") {
+      $('#'+user.username+'_battery').html(json.raw.battery+'% batt');
       $('#'+user.username+'_battery_img').show();
     }
     People.sort_by_last_time(users, user);
@@ -270,8 +270,8 @@ var People = {
 
 Date.prototype.setISO8601 = function(dString){
   var regexp = /(\d\d\d\d)(-)?(\d\d)(-)?(\d\d)(T)?(\d\d)(:)?(\d\d)(:)?(\d\d)(\.\d+)?(Z|([+-])(\d\d)(:)?(\d\d))/;
-  if (dString.toString().match(new RegExp(regexp))) {
-    var d = dString.match(new RegExp(regexp));
+  var d = dString.toString().match(new RegExp(regexp));
+  if (d) {
     var offset = 0;
     this.setUTCDate(1);
     this.setUTCFullYear(parseInt(d[1],10));
@@ -281,13 +281,13 @@ Date.prototype.setISO8601 = function(dString){
     this.setUTCMinutes(parseInt(d[9],10));
     this.setUTCSeconds(parseInt(d[11],10));
     if (d[12])
-    this.setUTCMilliseconds(parseFloat(d[12]) * 1000);
+      this.setUTCMilliseconds(parseFloat(d[12]) * 1000);
     else
-    this.setUTCMilliseconds(0);
+      this.setUTCMilliseconds(0);
     if (d[13] != 'Z') {
-    offset = (d[15] * 60) + parseInt(d[17],10);
-    offset *= ((d[14] == '-') ? -1 : 1);
-    this.setTime(this.getTime() - offset * 60 * 1000);
+      offset = (d[15] * 60) + parseInt(d[17],10);
+      offset *= ((d[14] == '-') ? -1 : 1);
+      this.setTime(this.getTime() - offset * 60 * 1000);
     }
   } else {
     this.setTime(Date.parse(dString));
